6fa0f443a801130de2d8e42b3fa1c7ba78b0794f
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-06-09 12:00:41 +0700

    edit code handle and UI bill

--- route/hoadon.js
+++ route/hoadon.js
@@ -3,13 +3,19 @@ const conn = require("../db/connect")
 const router = express.Router()
 router.use(express.static("views"))
 
-router.get("/danhsachhoadon", function(req, res){
+router.get("/danhsachhoadon", function (req, res) {
 	// let page = req.query.page;
 	// let vitribatdaulay = (page - 1)	* 5
-	let querysoluong = "SELECT hoadon.ID, hoadon.Ngay, khachhang.HoTen, hoadon.SoLuong, hoadon.ThanhTien, hoadon.TrangThai from hoadon join khachhang on hoadon.ID_KhachHang = khachhang.ID";
-	conn.query(querysoluong, function(err, result){
-        console.log(err);
-        res.render("hoadon/danhsachhoadon", {danhsachhoadon: result})
+	let querysoluong = `SELECT hoadon.ID, hoadon.Ngay, khachhang.HoTen, hoadon.SoLuong, hoadon.ThanhTien, hoadon.TrangThai 
+						from hoadon join khachhang on hoadon.ID_KhachHang = khachhang.ID`;
+						
+	conn.query(querysoluong, function (err, result) {
+		if (err) {
+			console.log();
+		} else {
+			res.render("hoadon/danhsachhoadon", { danhsachhoadon: result })
+		}
+
 		// let soluongtrang = result.length / 5
 		// if(err){
 		// 	console.log(err);
