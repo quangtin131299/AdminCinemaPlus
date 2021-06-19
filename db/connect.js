9ba64de1228d9c7599a87c10711a7cc43e5ec85b
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-06-20 00:24:59 +0700

    change infor connect db

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "datvephim",  
+    host: "bgcs6lfujsw9xwszjqhj-mysql.services.clever-cloud.com",
+    user: "ueogosnzkiuwtl5z",
+    password: "bgcs6lfujsw9xwszjqhj",
+    database: "ylaJ3jVOQ9oey9k25vF8",  
     port: 3306,
 });
 //Content Wrapper. Contains page content
