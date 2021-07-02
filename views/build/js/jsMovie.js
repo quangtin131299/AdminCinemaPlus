da8e5b1e49dc3ff877c93698bd12d6b4b753712c
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-07-02 22:24:24 +0700

    add validate movie

--- views/build/js/jsMovie.js
+++ views/build/js/jsMovie.js
@@ -1,3 +1,76 @@
+$('#formAddMovie').validate({
+    rules: {
+        txttenphim: {
+            required: true,
+            maxlength: 100
+        },
+        txtngaykhoichieu: {
+            required: true,
+        },
+        txtNgayKetThuc: {
+            required: true,
+        },
+        txtthoigian: {
+            required: true,
+        },
+        cboxtrangthai: {
+            required: true,
+        },
+        dropdownCountry: {
+            required: true,
+        },
+        dropdownNhaCungCap:{
+            required: true,
+        },
+        chbloai: {
+            required: true,
+        },
+        chbCinema: {
+            required: true,
+        }
+    },
+    messages: {
+        txttenphim: {
+            required: 'Tên phim không được bỏ trống',
+            maxlength: 'Hãy nhập tối đa 100 ký tự'
+        },
+        txtngaykhoichieu: {
+            required: 'Ngày khởi chiếu không được bỏ trống',
+        },
+        txtNgayKetThuc: {
+            required: 'Ngày kết thúc không được bỏ trống',
+        },
+        txtthoigian: {
+            required: 'Thời gian phim không được bỏ trống',
+        },
+        cboxtrangthai: {
+            required: 'Trạng thái chưa được chọn',
+        },
+        dropdownCountry: {
+            required: 'Quốc gia chưa được chọn',
+        },
+        chbloai: {
+            required: 'Loại chưa được chọn',
+        },
+        chbCinema: {
+            required: 'Rạp chiếu chưa được chọn',
+        },
+        dropdownNhaCungCap:{
+            required: 'Nhà cung cấp chưa được chọn',
+        }
+    },
+    errorPlacement: function(label, element) {
+        label[0].children[0].id = element[0].name;
+        
+        if(element[0].name == 'chbloai' || element[0].name == 'chbCinema'){
+            label.insertAfter( element.parent("div").parent('div').parent('div'));
+        }else{
+           label.insertAfter(element.parent("div"));
+        }     
+    },
+    wrapper: 'span'
+})
+
 let danhsachphim = [];
 document.getElementById("btnhuy").onclick = function () {
     window.location.replace("danhsachphim?page=1")
