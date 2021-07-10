fa78e8e2fe539a67ee2cbca83cad0822a2141fba
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-07-10 23:32:58 +0700

    mege branch array-schedule

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "datvephim",  
+    host: "by3lwtpeyft77fig1kwg-mysql.services.clever-cloud.com",
+    user: "ui0jmcww1c0x23v9",
+    password: "LCRJf2IGphiwLksj4FNQ",
+    database: "by3lwtpeyft77fig1kwg",  
     port: 3306,
 });
 //Content Wrapper. Contains page content
