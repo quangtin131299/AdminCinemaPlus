c5f1a668baa13f7caf7a2cfaa6d568c1061e671c
Thor Trinh <thor.t@itcgroup.xyz>
2021-07-16 00:53:55 +0700

    Change status ticket

--- route/soatve.js
+++ route/soatve.js
@@ -7,5 +7,20 @@ router.get("/hienthisoatve", function(req, res){
 	res.render("soatve/hienthisoatve");
 })
 
+router.post("/updateStatus", function(req, res){
+	let id = req.body.text;
+	let sqlquery = `update vedat join hoadon on vedat.ID_HoaDon = hoadon.ID
+					set vedat.TrangThai ='Đã nhận vé'
+					where hoadon.ID =?`;
+	conn.query(
+        sqlquery,[id],function (err, result) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.render("soatve/updateStatus");
+            }
+        }
+    );
+})
 
 module.exports = router
---
+++ views/build/js/jsScanQRcode.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    let mess = $('#modalTextMessage').html();
+
+    if(mess != ''){
+        $('#notifyModal').modal('show');
+    }
+    scanQRcode();
+})
+
+function scanQRcode() {
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
+    Instascan.Camera.getCameras().then(function(cameras){
+        if(cameras.length > 0 ){
+            scanner.start(cameras[0]);
+        } else{
+            alert('No cameras found');
+        }
+    }).catch(function(e) {
+        console.error(e);
+    });
+    scanner.addListener('scan',function(c){
+        document.getElementById('text').value=c;
+        let id = $('#text').val();
+        if (id != '') {
+            $.ajax({
+                method: 'POST',
+                url: '/soatve/updateStatus',
+                data: {
+                    id: id
+                },
+                success: function (data) {
+                    if (data) {
+                        hideLoading();
+                        $('#modalTextMessage').html(data.messNotify);
+                        $('#notifyModal').modal('show')
+                    }
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            })
+        }
+    });
+}
+
+$('#exampleModalCenter').on('hidden.bs.modal', function (e) {
+    $("#exampleModalCenter").modal('hide');
+})
+
+function hideLoading() {
+    $("#exampleModalCenter").modal('hide');
+}
+
+function showLoading(){
+   $('#exampleModalCenter').modal('show');
+}
