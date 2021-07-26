5bb5de78b69209662b0e3b890432a48ddf819de3
Thor Trinh <thor.t@itcgroup.xyz>
2021-07-26 11:21:56 +0700

    Add binding showtime

--- views/build/js/jsCalendar.js
+++ views/build/js/jsCalendar.js
@@ -6,51 +6,32 @@ $('#notifyModal').on('shown.bs.modal', function (e) {
     $("#exampleModalCenter").modal("hide");
 });
 
-/* initialize the external events
------------------------------------------------------------------*/
 function ini_events(ele) {
     ele.each(function () {
-        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-        // it doesn't need to have a start or end
+        
         var eventObject = {
-            title: $.trim($(this).text()) // use the element's text as the event title
+            title: $.trim($(this).text())
         }
-        // store the Event Object in the DOM element so we can get to it later
+        
         $(this).data('eventObject', eventObject)
-        // make the event draggable using jQuery UI
         $(this).draggable({
             zIndex: 1070,
-            revert: true, // will cause the event to go back to its
-            revertDuration: 0 //  original position after the drag
+            revert: true,
+            revertDuration: 0 
         })
     })
 }
 ini_events($('#external-events div.external-event'))
-/* initialize the calendar
------------------------------------------------------------------*/
-//Date for the calendar events (dummy data)
+
 var date = new Date()
 var d = date.getDate(),
     m = date.getMonth(),
     y = date.getFullYear()
 var Calendar = FullCalendar.Calendar;
-// var Draggable = FullCalendarInteraction.Draggable;
+
 var containerEl = document.getElementById('external-events');
 var calendarEl = document.getElementById('calendar');
-// initialize the external events
-// -----------------------------------------------------------------
-// new Draggable(containerEl, {
-//     itemSelector: '.external-event',
-//     eventData: function (eventEl) {
-//         console.log(eventEl);
-//         return {
-//             title: eventEl.innerText,
-//             backgroundColor: window.getComputedStyle(eventEl, null).getPropertyValue('background-color'),
-//             borderColor: window.getComputedStyle(eventEl, null).getPropertyValue('background-color'),
-//             textColor: window.getComputedStyle(eventEl, null).getPropertyValue('color'),
-//         };
-//     }
-// });
+
 let dtToday = new Date();
 let month = dtToday.getMonth() + 1;
 var day = dtToday.getDate();
@@ -73,54 +54,24 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
     },
     locale: 'vi',
 
-    //Random default events 
     height: "auto",
     editable: true,
-    droppable: true, // this allows things to be dropped onto the calendar !!!
+    droppable: true,
 });
 
 calendar.render();
 
-/* ADDING EVENTS */
-var currColor = '#3c8dbc' //Red by default
-//Color chooser button
+var currColor = '#3c8dbc' 
 var colorChooser = $('#color-chooser-btn')
 
 $('#color-chooser > li > a').click(function (e) {
     e.preventDefault()
-    //Save color
     currColor = $(this).css('color')
-    //Add color effect to button
     $('#add-new-event').css({
         'background-color': currColor,
         'border-color': currColor
     })
 })
-
-// $('#add-new-event').click(function (e) {
-//     e.preventDefault()
-//     //Get value and make sure it is not null
-//     var val = $('#new-event').val()
-//     if (val.length == 0) {
-//         return
-//     }
-//     //Create events
-//     var event = $('<div />')
-
-//     event.css({
-//         'background-color': currColor,
-//         'border-color': currColor,
-//         'color': '#fff'
-//     }).addClass('external-event')
-
-//     event.html(val)
-
-//     $('#external-events').prepend(event)
-//     //Add draggable funtionality
-//     ini_events(event)
-//     //Remove event from text input
-//     $('#new-event').val('')
-// })
 
 let movies = [];
 let rooms = [];
@@ -165,12 +116,8 @@ function loadMovieOfCinema(element) {
                                 borderColor: '#3c8dbc',
                             })
                         }
-
                     }
-
                 }
-
-
             }
         },
         error: function (erroSchedule) {
@@ -255,16 +202,6 @@ function calulatorEndTime(time, minuteMovie) {
 
     return `${hh}:${mm}:${ss}`;;
 }
-
-// function formatDateToServer() {
-//     let date = new Date();
-
-//     dd = date.getDate().toString().padStart(2, '0');
-//     mm = (date.getMonth() + 1).toString().padStart(2, '0');
-//     yyyy = date.getFullYear().toString().padStart(2, '0');
-
-//     return `${yyyy}-${mm}-${dd}`;
-// }
 
 function onSubmit() {
     let idCinema = $('select[name=dropdownCinema]').val();
