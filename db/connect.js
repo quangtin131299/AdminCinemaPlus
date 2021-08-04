6e61bdef9bccfa5eb02abb6d47d171d0dce79286
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-08-05 00:55:32 +0700

    change db

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,7 @@ const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "datvephim",  
+    database: "cinemaplus",  
     port: 3306,
 });
 //Content Wrapper. Contains page content
