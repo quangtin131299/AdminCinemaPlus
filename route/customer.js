35f909b2373f88d42bb78f88b642e9578a8d9b9a
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-08-05 19:46:45 +0700

    fix UI user

--- route/customer.js
+++ route/customer.js
@@ -7,18 +7,30 @@ router.use(express.static("views"))
 
 router.get("/danhsachuser", function(req, res){
 	let page = req.query.page;
-	let vitribatdaulay = (page - 1)	* 5
+	let vitribatdaulay = (page - 1)	* 10
 	let queryslcus = "select * from khachhang";
 	conn.query(queryslcus, function(err, resultkhach){
-		let ntrang = resultkhach.length / 5
+		let countCustomer = resultkhach.length
+		let ntrang = countCustomer / 10
 
 		if(err){
 			console.log(err);
 		}else{
-			let query = `SELECT khachhang.ID, khachhang.HoTen, khachhang.Email, DATE_FORMAT(khachhang.NgaySinh, '%d/%m/%Y') as 'NgaySinh', khachhang.SDT, khachhang.TaiKhoan, khachhang.MatKhau from khachhang Limit ${vitribatdaulay}, 5`;
+			let query = `SELECT khachhang.ID
+								, khachhang.HoTen
+								, khachhang.Email
+								, DATE_FORMAT(khachhang.NgaySinh, '%d/%m/%Y') as 'NgaySinh'
+								, khachhang.SDT
+								, khachhang.TaiKhoan
+								, khachhang.MatKhau 
+						FROM khachhang LIMIT ${vitribatdaulay}, 10`;
 			conn.query(query, function(err, result){	
+				if(err){
+					console.log(err);
+				}else{
+					res.render("customer/danhsachcustomer", {pagerespon: page,danhsachdatauser:result ,soluongtrang: Math.ceil(ntrang), countCustomerUse: countCustomer})	
+				}
 				
-				res.render("customer/danhsachcustomer", {pagerespon: page,danhsachdatauser:result ,soluongtrang: Math.ceil(ntrang)})	
 			})	
 		}
 		
