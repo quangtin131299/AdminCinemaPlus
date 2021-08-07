b53dd8247919513b94f7329ce871645bfe9991b6
Tiny Ngo <tiny.n@itcgroup.xyz>
2021-08-08 02:10:14 +0700

    statistical cinema

--- route/home.js
+++ route/home.js
@@ -20,6 +20,7 @@ router.get("/thongkephim", function (req, res){
         } else {
 
             let arrMovie = [];
+            
             for( let i = 0; i < result.length; i++ ){
                 
                 arrMovie.push({
@@ -27,10 +28,46 @@ router.get("/thongkephim", function (req, res){
                     doanhthuphim : result[i].DoanhThuPhim ? result[i].DoanhThuPhim : 0 ,
                 });
             }
+
             res.json(arrMovie);
         }
        
     });
 });
+
+router.get("/statisticalCinema", function(req, res){
+    let currentMonth = req.query.currentMonth;
+
+    let queryCinema = `SELECT * FROM rapphim`;
+
+    conn.query(queryCinema, function(errorCinema, resultStatistical){
+        if(errorCinema){
+            console.log(errorCinema);
+
+            res.json({statusCode: 0, message: 'Fail', resultStatistical: null});
+        } else {
+            let queryStatistical = `SELECT rapphim.ID
+                                            , rapphim.TenRap 
+                                            , COALESCE(SUM(hoadon.ThanhTienVe + COALESCE(hoadon_bapnuoc.ThanhTien, 0)), 0) as 'DoanhThu' 
+                                    FROM rapphim left join vedat on rapphim.ID = vedat.ID_Rap
+                                                        LEFT JOIN hoadon on hoadon.ID = vedat.ID_HoaDon
+                                                        LEFT JOIN hoadon_bapnuoc on hoadon_bapnuoc.ID_HoaDon = hoadon.ID
+                                    WHERE Month(hoadon.Ngay) = ? GROUP BY rapphim.TenRap`;
+
+            conn.query(queryStatistical, [currentMonth],function (error, result) {
+                if (error) {
+                    console.log(error);
+
+                    res.json({ statusCode: 0, message: 'Fail', resultCinema: resultStatistical, resultStatistical: null });
+                } else {
+
+                    res.json({ statusCode: 0, message: 'Fail', resultStatistical: result, resultCinema: resultStatistical});
+                }
+            })
+        }
+    }) 
+})
+
+
 
 module.exports = router;
